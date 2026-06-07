87c4beba6963882c5bd84293eaf54aa625d8db86
HydrolienF <hydrolien.f@gmail.com>
2026-06-07 20:50:50 +0200

    Remove publish from github action.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "net.mvndicraft.townyroads"
-version = "0.5.1"
+version = "0.5.2"
 description = "Add roads to Towny"
 var mainMinecraftVersion = "1.21.11"
 val supportedMinecraftVersions = "1.20 - 1.21.11"
