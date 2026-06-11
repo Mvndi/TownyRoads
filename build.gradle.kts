cc01384fc955692ed9f1bd333cc2bf9a1bd85ada
HydrolienF <hydrolien.f@gmail.com>
2026-06-11 20:15:24 +0200

    0.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "net.mvndicraft.townyroads"
-version = "0.6.0"
+version = "0.6.1"
 description = "Add roads to Towny"
 var mainMinecraftVersion = "1.21.11"
 val supportedMinecraftVersions = "1.20 - 1.21.11"
